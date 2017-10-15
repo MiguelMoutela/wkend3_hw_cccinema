4289e15ea15af10ddb09fad926539f44a9f7eaa0
Miguel <miguelmoutela@hotmail.com>
2017-10-15 15:15:34 +0100

    added some screenings to the console.rb

--- console.rb
+++ console.rb
@@ -29,5 +29,18 @@ ticket_1.save()
 ticket_2 = Ticket.new({'customer_id' => customer_2.id, 'movie_id' => movie_2.id})
 ticket_2.save()
 
+
+screening_1 = Screening.new({'movie_id' => movie_1.id, 'time' => '16:00:00'})
+
+screening_2 = Screening.new({'movie_id' => movie_1.id, 'time' => '21:00:00'})
+
+screening_3 = Screening.new({'movie_id' => movie_1.id, 'time' => '21:00:00'})
+
+screening_4 = Screening.new({'movie_id' => movie_2.id, 'time' => '16:00:00'})
+
+screening_5 = Screening.new({'movie_id' => movie_2.id, 'time' => '21:00:00'})
+
+screening_6 = Screening.new({'movie_id' => movie_2.id, 'time' => '21:00:00'})
+
 binding.pry()
 nil
--- models/screening.rb
+++ models/screening.rb
@@ -11,4 +11,6 @@ class Screening
     @screening_time = options['screening_time']
   end
 
+  
+
 end
