17a825409d49f40fa7e20ac48123e3be0be7c658
Miguel <miguelmoutela@hotmail.com>
2017-10-15 14:54:24 +0100

    def init on screening class

--- models/screening.rb
+++ models/screening.rb
@@ -1,4 +1,14 @@
 require_relative('..db/sql_runner.rb')
+
 class Screening
+
+  attr_reader(:id)
+  attr_accessor(:movie_id, :screening_time)
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id'] != nil
+    @movie_id = options['movie_id']
+    @screening_time = options['screening_time']
+  end
 
 end
