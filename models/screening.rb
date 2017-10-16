82a33eefa24ade5318eff3cbcb2a45ad98cfd5d9
Miguel <miguelmoutela@hotmail.com>
2017-10-16 10:09:34 +0100

    last commit to submit

--- models/screening.rb
+++ models/screening.rb
@@ -1,9 +1,10 @@
 require_relative('..db/sql_runner.rb')
+require_relative('./ticket.rb')
 
 class Screening
 
   attr_reader(:id)
-  attr_accessor(:movie_id, :screening_time)
+  attr_accessor(:movie_id, :screening_time, :tickets_bought)
 
   def initialize(options)
     @id = options['id'].to_i if options['id'] != nil
@@ -11,6 +12,6 @@ class Screening
     @screening_time = options['screening_time']
   end
 
-  
+
 
 end
